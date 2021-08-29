24aae7f817e33fa6710dfd78de028acaac22915a
Saffy <72629019+Saffy11-hub@users.noreply.github.com>
2021-08-29 14:56:28 +0100

    Update models.py

--- backend/noticeboard/notice/models.py
+++ backend/noticeboard/notice/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Aquinas(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
