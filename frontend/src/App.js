f2089b9408615f8e326d3d87342838c8df8eba9d
unknown <kayodefredrickbalogun@gmail.com>
2021-08-30 16:10:18 +0100

    #144 convert plain html to the js framework

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Header from './components/Header/Header';
+import Notice from './components/NoticeBoard/Notice';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <h1>1 Notice Board Plugin</h1>
-    </div>
+      <div className="app__body">
+        <Sidebar />
+        <Header />
+      </div>
+   </div>
   );
 }
 
