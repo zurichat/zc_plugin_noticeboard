d4e89541b56be8ddf801adcc913332c71590df32
Saffy <72629019+Saffy11-hub@users.noreply.github.com>
2021-08-30 01:28:33 +0100

    This fixes #218
    Created a todolist for a user, It contains both name and items.

--- backend/notice_project/account/models.py
+++ backend/notice_project/account/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+# Create the name of a todolist
+class ToDoList(models.Model):
+  name = models.CharField(max_length=100)
+  
+  def __str__(self):
+    return self.name
+  
+
+# Create the item of a todolist  
+class Item(models.Model):
+  todolist = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+  text = models.CharField(max_length=300)
+  complete = models.BooleanField()
+  date = models.DateTimeField(auto_now_add=True)
+  
+  def __str__(self):
+    return self.text
+
+
+
+
+
+
+
+
