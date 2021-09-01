9643bb098c14b64bf90339ee54c380253f17ae84
“mubarak-bunza” <mubarakabubakar227@gmail.com>
2021-09-01 04:27:34 +0100

    edit noticeboard component

--- frontend/src/components/NoticeBoard/NoticeBoard.jsx
+++ frontend/src/components/NoticeBoard/NoticeBoard.jsx
@@ -6,6 +6,7 @@ import axios from "axios"
 function NoticeBoard() {
     return (
         <div className="notice">
+            <h3>Endpoints</h3>
             {axios.get("/api/endpoints/")}
         </div>
     )
