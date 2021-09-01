9978f6390e2c9e2902d4004642586d58779da7ae
“mubarak-bunza” <mubarakabubakar227@gmail.com>
2021-09-01 10:03:37 +0100

    edit noticeboard component

--- frontend/src/components/NoticeBoard/NoticeBoard.jsx
+++ frontend/src/components/NoticeBoard/NoticeBoard.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import "./NoticeBoard.css"
-import axios from "axios"
+import React from 'react';
+import "./NoticeBoard.css";
+import axios from "axios";
 
 
 function NoticeBoard() {
+    const [endpoint, setEndpoint] = React.useState(null);
+
+    React.useEffect(() => {
+        axios.get('/api/endpoints').then((response) => {
+            setEndpoint(response.data);
+        });
+    }, []);
+
+    if (!endpoint) return null;
+    const endpoints = Object.keys(endpoint).map((item)=>endpoint[item])
+    
     return (
         <div className="notice">
-            <h3>Endpoints</h3>
-            {axios.get("/api/endpoints/")}
+            <h3>Endpoints From Backend</h3>
+
+            {endpoints.map((item, i)=>{
+                return <li key={i}>{item}</li>
+            })}
         </div>
     )
 }
