6021f973a7f9edfc181d1c01e4cee10982ef3f86
“mubarak-bunza” <mubarakabubakar227@gmail.com>
2021-09-01 04:22:12 +0100

    install axios

--- frontend/src/components/NoticeBoard/NoticeBoard.jsx
+++ frontend/src/components/NoticeBoard/NoticeBoard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./NoticeBoard.css"
+import axios from "axios"
+
 
 function NoticeBoard() {
     return (
         <div className="notice">
-            
+            {axios.get("/api/endpoints/")}
         </div>
     )
 }
