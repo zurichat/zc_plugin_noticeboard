17b2574ffdbad28c471a3c0378c246cc567c8018
Kingsley Solomon <kinxly@gmail.com>
2021-09-02 11:46:58 +0100

    #253 admin notice view(page)

--- frontend/src/components/NoticeBoard/NoticeBoard.jsx
+++ frontend/src/components/NoticeBoard/NoticeBoard.jsx
@@ -1,29 +1,15 @@
-import React from 'react';
-import "./NoticeBoard.css";
-import axios from "axios";
-
+import React from 'react'
+import './NoticeBoard.css'
+import NoticeBoardHeader from './NoticeBoardHeader'
+import AdminNotice from './AdminNoticeBoardComponents/AdminNotice'
 
 function NoticeBoard() {
-    const [endpoint, setEndpoint] = React.useState(null);
-
-    React.useEffect(() => {
-        axios.get('/api/endpoints').then((response) => {
-            setEndpoint(response.data);
-        });
-    }, []);
-
-    if (!endpoint) return null;
-    const endpoints = Object.keys(endpoint).map((item)=>endpoint[item])
-    
-    return (
-        <div className="notice">
-            <h3>Endpoints From Backend</h3>
-
-            {endpoints.map((item, i)=>{
-                return <li key={i}>{item}</li>
-            })}
-        </div>
-    )
+  return (
+    <div className='notice'>
+      <NoticeBoardHeader />
+      <AdminNotice />
+    </div>
+  )
 }
 
 export default NoticeBoard
