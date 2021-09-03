a227a6c5cb76bb489bef7eb4cc5a40880cb6722f
Daniel Iheonu <64360587+danieliheonu@users.noreply.github.com>
2021-09-03 20:20:19 +0100

    Revert "Obintus"

--- backend/notice_project/notice/views.py
+++ backend/notice_project/notice/views.py
@@ -1,92 +1,50 @@
-from rest_framework import views
-from rest_framework import status
-from rest_framework.response import Response
-from .serializers import CreateNoticeSerializer, CommentReactionSerializer
-import requests
+from django.shortcuts import render
+from django.http import JsonResponse
 
-class AllNoticesView(views.APIView):
+# Create your views here.
 
-    """GET request to display/retrieve all existing notices"""
-    def get(self, request):
-        data= [
-        {"title":"Management meeting",
-        "text":"Management has updated the design scedule",
-        "photo_url":"null",
-        "video_url":"null",
-        "audio_url":"null"},
+def home(request):
+    pass
 
-        {"title":"Stage 5",
-        "text":"Complete a ticket to move to stage 5",
-        "photo_url":"null",
-        "video_url":"null",
-        "audio_url":"null",
-        "published":"True"},
+def endpoints(request):
+    data = {
+        "viewNotice": "http://localhost:8000/api/viewNotice/",
+        "sendNotice": "http://localhost:8000/api/sendNotice/",
+        "editTimsestamp": "http://localhost:8000/api/setNoticeTimestamp/",
+        "endpoints": "http://localhost:8000/api/endpoints/",
+    }
 
-        {"title":"Individual work",
-        "text":"Each intern is expected to complete at least one ticket individually",
-        "photo_url":"null",
-        "video_url":"null",
-        "audio_url":"null"},
-        ]
-        
-        results = CreateNoticeSerializer(data, many=True).data
-        return Response(results, status=status.HTTP_200_OK)
+    return JsonResponse(data, status=200)
+
+def viewNotice(request):
+    data = {
+        "id": 1,
+        "username": "Bruce Wayne",
+        "date":"24 Hours ago",
+        "timestamp": "3 Hours ago",
+        "views": "21",
+        "likes": "12",
+        "title": "App Testing Event",
+        "info": ""
+    }
+
+    return JsonResponse(data, status=200)
 
 
-class CreateNoticeView(views.APIView):
+def sendNotice(request):
+    data = {
+        "username": "Daniel",
+        "recipient": "ZetsuArmy",
+        "title": "Does It Work?",
+        "fileUploaded": "",
+        "message": "Yes, It Works!!!"
+    }
 
-    def post(self, request):
-        serializer = CreateNoticeSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
+    return JsonResponse(data, status=200)
 
-            payload={
-                    "plugin_id": "612a3a914acf115e685df8e3",
-                    "organization_id": "id",
-                    "collection_name": "mycollection",
-                    "bulk_write": False,
-                    "object_id": "1212",
-                    "filter": {},
-                    "payload": serializer.data
-                    }
-            external_api_url = 'https://zccore.herokuapp.com/data/write'
-            
-            res = requests.post(external_api_url, payload)
-            return Response({"post_data":payload, "server_response":res.json()}, status=status.HTTP_201_CREATED)
-
-
-           
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-  
-class CommentReactionAPIView(views.APIView):
-
-    def put(self, request):
-        serializer = CommentReactionSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({
-                "success": True,
-                "data": serializer.data,
-                "message": "Your have successfully updated your reaction"
-            })
-        return Response({
-                "success": False,
-                "data": serializer.data,
-                "message": "Your reaction could not be updated"
-            })
-
-
-    def patch(self, request):
-        serializer = CommentReactionSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({
-                "success": True,
-                "data": serializer.data,
-                "message": "Your have successfully updated your reaction"
-            })
-        return Response({
-                "success": False,
-                "data": serializer.data,
-                "message": "Your reaction could not be updated"
-            })
+def setNoticeTimestamp(request):
+    data = {
+        "timestamp": "3 Hours ago"
+    }
+    
+    return JsonResponse(data, status=200)
--- frontend/src/components/NoticeBoard/NoticeBoard.jsx
+++ frontend/src/components/NoticeBoard/NoticeBoard.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
-import './NoticeBoard.css'
-import NoticeBoardHeader from './NoticeBoardHeader'
-import AdminNotice from '../NoticeBoard/noticeBoardComponent/AdminNotice'
+import React from 'react';
+import "./NoticeBoard.css";
+import axios from "axios";
+
 
 function NoticeBoard() {
-  return (
-    <div className='notice'>
-      <NoticeBoardHeader />
-      <AdminNotice />
-    </div>
-  )
+    const [endpoint, setEndpoint] = React.useState(null);
+
+    React.useEffect(() => {
+        axios.get('/api/endpoints').then((response) => {
+            setEndpoint(response.data);
+        });
+    }, []);
+
+    if (!endpoint) return null;
+    const endpoints = Object.keys(endpoint).map((item)=>endpoint[item])
+    
+    return (
+        <div className="notice">
+            <h3>Endpoints From Backend</h3>
+
+            {endpoints.map((item, i)=>{
+                return <li key={i}>{item}</li>
+            })}
+        </div>
+    )
 }
 
 export default NoticeBoard
