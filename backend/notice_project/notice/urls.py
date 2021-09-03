00870d50590bc7fe84e70f26ee161fdfa8b1460c
Smyja <akposlive59@gmail.com>
2021-09-03 19:09:20 +0100

    Url

--- backend/notice_project/notice/urls.py
+++ backend/notice_project/notice/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import CreateNoticeView, CommentReactionAPIView, AllNoticesView, delete, deleteNotice
-
+from .views import CreateNoticeView, CommentReactionAPIView, AllNoticesView, delete, deleteNotice,update_notice
 #add url routes here
 
 urlpatterns = [
@@ -10,8 +9,11 @@ urlpatterns = [
     path('all-notices', AllNoticesView.as_view()),
 
     path('comment/reaction/update', CommentReactionAPIView.as_view()),
+
+    path('editnotice/<int:notice_id>/update/', update_notice),
     
     path('delete/', delete, name="delete"),
 
       path('delete-notice', deleteNotice, name='delete-notice'),
+
 ]
