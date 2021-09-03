05b257481eac27b0ae54e9e9fd739798f5183c41
unknown <kayodefredrickbalogun@gmail.com>
2021-09-03 16:43:49 +0100

    #311 routing between the CreateNotice, ViewNotice and Adminnotice screens by ZetsuArmy

--- frontend/src/components/NoticeBoard/noticeBoardComponent/CreateNotice.jsx
+++ frontend/src/components/NoticeBoard/noticeBoardComponent/CreateNotice.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function CreateNotice() {
     return (
         <div className="create-notice">
-            This is the create notice screen
+            <h1>This is the create notice screen</h1>
+            <p>Code with care thank you...</p>
+
         </div>
     )
 }
