ae784e1cce3eda895f06b27bf03c68c678553df1
BoriEco <oola@ecobank.com>
2021-09-03 09:11:53 +0100

    merging

--- backend/notice_project/notice/views.py
+++ backend/notice_project/notice/views.py
@@ -1,22 +1,91 @@
+
 from rest_framework import views
 from rest_framework import status
 from rest_framework.response import Response
+
+import requests
 from .serializers import CreateNoticeSerializer, CommentReactionSerializer
+
+from django.shortcuts import render
+from django.http import JsonResponse
+
+
 
 
 # Create your views here.
+
 class CreateNoticeView(views.APIView):
 
     def post(self, request):
         serializer = CreateNoticeSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
+
+            payload={
+                    "plugin_id": "612a3a914acf115e685df8e3",
+                    "organization_id": "id",
+                    "collection_name": "mycollection",
+                    "bulk_write": False,
+                    "object_id": "1212",
+                    "filter": {},
+                    "payload": serializer.data
+                    }
+            external_api_url = 'https://zccore.herokuapp.com/data/write'
+            
+            res = requests.post(external_api_url, payload)
+            return Response({"post_data":payload, "server_response":res.json()}, status=status.HTTP_201_CREATED)
+            # return Response(res.json(), status=status.HTTP_201_CREATED)
+            # return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+ 
+    
+def home(request):
+    pass
+
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CommentReactionAPIView(views.APIView):
 
+
+def viewNotice(request):
+    data = {
+        "id": 1,
+        "username": "Bruce Wayne",
+        "date":"24 Hours ago",
+        "timestamp": "3 Hours ago",
+        "views": "21",
+        "likes": "12",
+        "title": "App Testing Event",
+        "info": ""
+    }
+
+    return JsonResponse(data, status=200)
+
+
+def sendNotice(request):
+    data = {
+        "username": "Daniel",
+        "recipient": "ZetsuArmy",
+        "title": "Does It Work?",
+        "fileUploaded": "",
+        "message": "Yes, It Works!!!"
+    }
+
+    return JsonResponse(data, status=200)
+
+def setNoticeTimestamp(request):
+    data = {
+        "timestamp": "3 Hours ago"
+    }
+    
+    return JsonResponse(data, status=200)
+
+>>>>>>> user can update their comment reaction resolves #256 (#275)
     def put(self, request):
         serializer = CommentReactionSerializer(data=request.data)
         if serializer.is_valid():
@@ -29,7 +98,17 @@ class CommentReactionAPIView(views.APIView):
         return Response({
                 "success": False,
                 "data": serializer.data,
+
                 "message": "Your reaction could not be updated"
+=======
+                "message": "Your hreaction could not be updated"
+>>>>>>> user can update their comment reaction resolves #256 (#275)
+=======
+                "message": "Your reaction could not be updated"
+>>>>>>> updated response message
+=======
+                "message": "Your reaction could not be updated"
+>>>>>>> updated response message
             })
 
 
