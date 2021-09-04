819df9bc43bf70869dfc3ac336c9ff25ebee41d4
Daniel Iheonu <64360587+danieliheonu@users.noreply.github.com>
2021-09-04 09:17:15 +0100

    Revert "Grace"

--- backend/notice_project/notice/models.py
+++ backend/notice_project/notice/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+# from django.db import models
 
 class Notice:
     def __init__(self, title,
--- backend/notice_project/noticeboard/urls.py
+++ backend/notice_project/noticeboard/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
 
     path('admin/', admin.site.urls),
 
     path('api/', include('notice.urls')),
+    # path('', TemplateView.as_view(template_name = "index.html")),
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html')), 
     
 ]
