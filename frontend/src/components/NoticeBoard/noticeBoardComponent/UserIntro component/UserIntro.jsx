62dc713243b3885df159b08ce4f4b8004bc0a81c
Emmanuel Charles <emmanuelcharles2133@gmail.com>
2021-09-05 14:32:31 +0100

    issue number #362 by @ Manuel on slack

--- frontend/src/components/NoticeBoard/noticeBoardComponent/UserIntro component/UserIntro.jsx
+++ frontend/src/components/NoticeBoard/noticeBoardComponent/UserIntro component/UserIntro.jsx
@@ -2,7 +2,6 @@ import { Button } from '@material-ui/core'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './UserIntro.css'
-import { Link } from 'react-router-dom'
 import defaultEdit from './default.svg'
 
 const UserIntro = () => {
@@ -12,11 +11,7 @@ const UserIntro = () => {
         <div className="IntroImg">
           <img className="Img" alt="zuri" src={require("../../../../assets/Illustration.svg").default} />
         </div>
-        <div className='UserIntroPdesktop'>
-          <p>👋  Hey you have been invited to the notice board, create notices for</p>
-          <span>the workspace and different channels and you can pin important</span>
-          <span>notices to help everyone identify them.</span>
-        </div>
+        
         <div className='UserIntroPmobile'>
           <p>👋  Hey you have been invited to the notice<br/>board, create notices for
           the workspace and different<br/>channels and you can pin important notices to help<br/>everyone identify them.</p>
