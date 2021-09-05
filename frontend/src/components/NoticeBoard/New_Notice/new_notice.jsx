58e145dd7918e452df36a46b33ce049b76a3bd8c
Joshua4221 <42146616+Joshua4221@users.noreply.github.com>
2021-09-05 09:58:27 +0100

    new notice field

--- frontend/src/components/NoticeBoard/New_Notice/new_notice.jsx
+++ frontend/src/components/NoticeBoard/New_Notice/new_notice.jsx
@@ -5,19 +5,19 @@ const NewNotice = () => {
     return (
         <div className="new-notice-body">
             <div className="new-notice-main">
-                <h3 className="new-notice-header">Create New Notice</h3>
+                {/* <h3 className="new-notice-header">Create New Notice</h3> */}
                 <div className="new-notice">
                     <div className="sub-new-notice">
-                        <label htmlFor="">Subject:</label><br />
+                        <label htmlFor="">Title/Subject:</label><br />
                         <input type="text" placeholder="Enter the subject of your notice"/>
                     </div>
 
                     <div className="sub-new-notice">
-                        <label htmlFor="">Description:</label><br />
+                        <label htmlFor="">To:</label><br />
                         <select name="" id="">
-                            <option value="">Subject notice recepiants</option>
-                            <option value=""></option>
-                            <option value=""></option>
+                            <option value="">Mark</option>
+                            <option value="">Steve</option>
+                            <option value="">James</option>
                         </select>
                     </div>
                 </div>
