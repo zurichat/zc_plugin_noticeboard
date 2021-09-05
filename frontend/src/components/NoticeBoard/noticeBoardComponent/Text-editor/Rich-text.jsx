9cdd38c6e7b2d81e42fb2ed84a80cefe4857564a
Daniel Iheonu <64360587+danieliheonu@users.noreply.github.com>
2021-09-05 12:51:29 +0100

    Revert "Revert "solved user notice import error""

--- frontend/src/components/NoticeBoard/noticeBoardComponent/Text-editor/Rich-text.jsx
+++ frontend/src/components/NoticeBoard/noticeBoardComponent/Text-editor/Rich-text.jsx
@@ -1,23 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./Text-editor.css";
-import { EditorState } from 'draft-js';
-import { Editor } from 'react-draft-wysiwyg';
-import '../../../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import { EditorState } from "draft-js";
+import { Editor } from "react-draft-wysiwyg";
+import "../../../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import imageIcon from "./icons/attachment.svg";
-import mentionIcon from "./icons/mentionAdder.svg"
-import {MentionAdder, ToggleToolbar} from "./Text_editor_features"
-
+import mentionIcon from "./icons/mentionAdder.svg";
+import { MentionAdder, ToggleToolbar } from "./Text_editor_features";
 
 // This is the editor Component class
 class TextEditor extends Component {
-  constructor(props){
-      super(props)
-      this.state = {
-      editorState: EditorState.createEmpty()
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      editorState: EditorState.createEmpty(),
+    };
   }
 
-  onEditorStateChange =(editorState) => {
+  onEditorStateChange = (editorState) => {
     this.setState({
       editorState,
     });
@@ -31,61 +30,63 @@ class TextEditor extends Component {
         <label>Message:</label>
 
         <Editor
-          placeholder= "Enter the content of your notice"
+          placeholder="Enter the content of your notice"
           wrapperClassName="text-editor"
           editorClassName="textarea"
           toolbarClassName="toolbarClass"
           onEditorStateChange={this.onEditorStateChange}
           toolbarCustomButtons={[<MentionAdder />, <ToggleToolbar />]}
-          
-
           toolbar={{
-            options: ['fontSize', 'inline', 'list', 'textAlign', 'link', 'image', 'emoji'],
+            options: [
+              "fontSize",
+              "inline",
+              "list",
+              "textAlign",
+              "link",
+              "image",
+              "emoji",
+            ],
             inline: {
-              options: ['bold', 'italic', 'underline', 'strikethrough'],
-              className: "rdw-invisible"
+              options: ["bold", "italic", "underline", "strikethrough"],
+              className: "rdw-invisible",
             },
             fontSize: {
-              className: "rdw-invisible"
+              className: "rdw-invisible",
             },
             link: {
               className: "rdw-invisible",
-              options: ['link'],
+              options: ["link"],
             },
             textAlign: {
               className: "rdw-invisible",
             },
             list: {
               className: "rdw-invisible",
-              options: ['unordered', 'ordered']
+              options: ["unordered", "ordered"],
             },
-            emoji: {
-              
-            },
+            emoji: {},
             image: {
               icon: imageIcon,
               uploadEnabled: true,
               urlEnabled: true,
               uploadCallback: this.uploadImageCallback,
-              inputAccept: 'image/gif,image/jpeg,image/jpg,image/png,image/svg',
-            }
+              inputAccept: "image/gif,image/jpeg,image/jpg,image/png,image/svg",
+            },
           }}
-
           mention={{
-            separator: ' ',
-            trigger: '@',
+            separator: " ",
+            trigger: "@",
             suggestions: [
-              { text: 'APPLE', value: 'apple', url: 'apple' },
-              { text: 'BANANA', value: 'banana', url: 'banana' },
-              { text: 'CHERRY', value: 'cherry', url: 'cherry' },
-              { text: 'DURIAN', value: 'durian', url: 'durian' },
-              { text: 'EGGFRUIT', value: 'eggfruit', url: 'eggfruit' },
-              { text: 'FIG', value: 'fig', url: 'fig' },
-              { text: 'GRAPEFRUIT', value: 'grapefruit', url: 'grapefruit' },
-              { text: 'HONEYDEW', value: 'honeydew', url: 'honeydew' },
+              { text: "APPLE", value: "apple", url: "apple" },
+              { text: "BANANA", value: "banana", url: "banana" },
+              { text: "CHERRY", value: "cherry", url: "cherry" },
+              { text: "DURIAN", value: "durian", url: "durian" },
+              { text: "EGGFRUIT", value: "eggfruit", url: "eggfruit" },
+              { text: "FIG", value: "fig", url: "fig" },
+              { text: "GRAPEFRUIT", value: "grapefruit", url: "grapefruit" },
+              { text: "HONEYDEW", value: "honeydew", url: "honeydew" },
             ],
           }}
-
         />
       </div>
     );
