f26c3437118d96c4578f07a90741e3fdf6c0fdab
Franklin-Ominyi <franklinchibuikeominyi@gmail.com>
2021-09-06 09:13:25 -0700

    I created a styled component for the PMobile @Chitech

--- frontend/src/components/NoticeBoard/noticeBoardComponent/UserIntro component/UserIntro.jsx
+++ frontend/src/components/NoticeBoard/noticeBoardComponent/UserIntro component/UserIntro.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 import './UserIntro.css'
 import defaultEdit from './default.svg'
 import { IntroButtonStyles } from './styledComponents/IntroButtonStyles'
+import { IntroPMobileStyles } from './styledComponents/IntroPMobileStyles'
 
 const UserIntro = () => {
   return (
@@ -13,10 +14,10 @@ const UserIntro = () => {
           <img className="Img" alt="zuri" src={require("../../../../assets/Illustration.svg").default} />
         </div>
         
-        <div className='UserIntroPmobile'>
-          <p>👋  Hey you have been invited to the notice<br/>board, create notices for
-          the workspace and different<br/>channels and you can pin important notices to help<br/>everyone identify them.</p>
-        </div>
+        <IntroPMobileStyles>
+         <p>👋  Hey you have been invited to the notice<br/>board, create notices for
+         the workspace and different<br/>channels and you can pin important notices to help<br/>everyone identify them.</p>
+        </IntroPMobileStyles>
         <Link to="/admin-notice"><IntroButtonStyles label="View Notice"  variant="contained">View Notice <img src={defaultEdit} alt='create notice' /></IntroButtonStyles></Link>
      </div>
     </div>
