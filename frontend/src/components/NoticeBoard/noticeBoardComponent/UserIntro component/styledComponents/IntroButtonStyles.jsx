beed75320e42b52e2470af8ded74f4a481834836
dejiborewa <ayodejiakinborewa@gmail.com>
2021-09-08 15:52:51 +0100

    updates

--- frontend/src/components/NoticeBoard/noticeBoardComponent/UserIntro component/styledComponents/IntroButtonStyles.jsx
+++ frontend/src/components/NoticeBoard/noticeBoardComponent/UserIntro component/styledComponents/IntroButtonStyles.jsx
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 export const IntroButtonStyles = styled.button`
 	background: var(--accent-color) !important;
