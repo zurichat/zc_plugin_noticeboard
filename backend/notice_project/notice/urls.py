59ef1738aa578c0e83e1babdd589d25c2bff01d4
Daniel Iheonu <64360587+danieldutcum@users.noreply.github.com>
2021-09-09 20:31:50 +0100

    added sidebar feature

--- backend/notice_project/notice/urls.py
+++ backend/notice_project/notice/urls.py
@@ -1,27 +1,8 @@
 from django.urls import path
-from .views import CreateNoticeView, CommentReactionAPIView, AllNoticesView, CommentDeleteAPIView, NoticeDeleteAPIView, \
-    EditNoticeAPIView, RetrieveNoticeCommentsView, CommentCreateAPIView,NoticeDetailAPIView
+from .views import sidebar
 
 # add url routes here
 
 urlpatterns = [
-
-    path('notices/', CreateNoticeView.as_view()),
-
-    path('all-notices', AllNoticesView.as_view()),
-
-    path('comment/reaction/update', CommentReactionAPIView.as_view()),
-
-    path('notice/update', EditNoticeAPIView.as_view()),
-
-    path('comment/delete', CommentDeleteAPIView.as_view()),
-
-    path('notice/delete', NoticeDeleteAPIView.as_view()),
-
-    path('comment/get', RetrieveNoticeCommentsView.as_view()),
-
-    path('comment/create', CommentCreateAPIView.as_view()),
-
-    path('notice/<int:notice_id>/details', NoticeDetailAPIView.as_view()),
-
+    path('sidebar', sidebar),
 ]
