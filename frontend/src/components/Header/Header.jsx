f224cc7ebdee0eb425bda9106c0eea2ef19774f8
droxy <chiomaotu22@gmail.com>
2021-09-10 10:10:09 +0100

    added and styled  noticeboard-header component

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
       <div className="header__wraps">
         <div className="header__hamburger-icon">
           <svg
-            width="18"
-            height="18"
+            width="20"
+            height="20"
             viewBox="0 0 18 18"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
