f6ad988698ffb579a6b6484f8f64d8ccabb9cca8
Daniel Iheonu <64360587+danieldutcum@users.noreply.github.com>
2021-09-11 22:49:56 +0100

    rectified

--- backend/notice_project/notice/urls.py
+++ backend/notice_project/notice/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import CreateNoticeView, CommentReactionAPIView, AllNoticesView, CommentDeleteAPIView, NoticeDeleteAPIView, \
-    EditNoticeAPIView, RetrieveNoticeCommentsView, CommentCreateAPIView,NoticeDetailAPIView,install,store_notice
+from .views import install,store_notice,sidebar
 
 # add url routes here
 
@@ -8,6 +7,7 @@ urlpatterns = [
     path('sidebar', sidebar, name="sidebar"),
 
     path('install',install, name='install'),
+
     path('save',store_notice, name='store_notice'),
 
 ]
--- backend/notice_project/notice/views.py
+++ backend/notice_project/notice/views.py
@@ -4,7 +4,7 @@ from .serializers import CreateNoticeSerializer, CommentReactionSerializer, Edit
     CommentCreateSerializer, CreateReactionSerializer,CreateNoticeV2Serializer
 import requests
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework import views, status
 from .storage import Database
 
 class AllNoticesView(views.APIView):
@@ -84,8 +84,35 @@ class CreateNoticeView(views.APIView):
                 "filter": {},
                 "payload": serializer.data
             }
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def sidebar(request):
+    sidebar = {
+                "name" : "Noticeboard Plugin",
+                "description" : "Displays Information On A Noticeboard",
+                "plugin_id" : "6139ca8d59842c7444fb01fe",
+                "organisation_id" : "FRYIUOHF",
+                "user_id" : "613a1e9259842c7444fb0225",
+                "group_name" : "Noticeboard",
+                "show_group" : False,
+                "joined_rooms": [],
+                "public_rooms": [
+                    {
+                        "title": "jokes",
+                        "id": "DFGfH-EDDDDS-DFDDF",
+                        "unread": 342,
+                        "members": 32,
+                        "icon" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRr-kPo-vAmp_GrCZbnmqT6PMU5Wi5BLwgvPQ&usqp=CAU",
+                        "action" : "open",
+                        "auto-join" : True
+                    }
+                ]
+            }
     return Response(sidebar, status=status.HTTP_200_OK)
 
+    
 def install(request):
      install = {
         "name" : "Noticeboard Plugin",
