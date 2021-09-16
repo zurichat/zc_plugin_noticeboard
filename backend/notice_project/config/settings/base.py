8b6d6356da019f482670ab2cc83b82a2a5d4fd29
Daniel Iheonu <64360587+danieldutcum@users.noreply.github.com>
2021-09-16 10:29:31 +0100

    changed the cors allowed origins

--- backend/notice_project/config/settings/base.py
+++ backend/notice_project/config/settings/base.py
@@ -135,4 +135,6 @@ STATIC_ROOT = os.path.join(BASE_DIR,'build', "static")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 PLUGIN_ID=""
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "*",
+]
