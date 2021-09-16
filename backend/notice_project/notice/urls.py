f9e4ffaec04cf6ca47a594fd3e15eeaf8ac42ee1
Daniel Iheonu <64360587+danieldutcum@users.noreply.github.com>
2021-09-16 22:44:36 +0100

    created a docs endpoint

--- backend/notice_project/notice/urls.py
+++ backend/notice_project/notice/urls.py
@@ -1,20 +1,32 @@
 from django.urls import path
-from .views import install,sidebar, CreateNewNotices, NoticeAPI, UpdateNoticeAPIView, DeleteNotice, search
+from .views import install,sidebar, create_room, CreateNewNotices, search
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
-# add url routes here
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Noticeboard API",
+      default_version='v1',
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    path('sidebar', sidebar, name="sidebar"),
+   
+   path('sidebar', sidebar, name="sidebar"),
 
-    path('install',install, name='install'),
+   path('install',install, name='install'),
 
-    path('create-notice', CreateNewNotices.as_view()),
+   path('create-notice-room', create_room),
 
-    path('update', UpdateNoticeAPIView.as_view()),
+   path('create-notice', CreateNewNotices.as_view()),
 
-    path('view', NoticeAPI.as_view()),
+   path('search', search.as_view()),
 
-    path('delete', DeleteNotice.as_view()),
+   path('docs', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
-    path('search', search.as_view())
 ]
