8087fdd324c7824516603635a0b5eb7dee8b3ac0
clefayomide <victorayomide32@gmail.com>
2021-09-16 20:32:43 +0100

    migration

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "zuri-chat",
+    projectName: "plugin-noticeboard",
+    webpackConfigEnv,
+    argv,
+  });
+
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+    // module: {
+    //   rules: [
+    //     {
+    //       test: /\.css$/i,
+    //       use: ["style-loader", "css-loader"],
+    //     },
+    //   ],
+    // },
+  });
+};
