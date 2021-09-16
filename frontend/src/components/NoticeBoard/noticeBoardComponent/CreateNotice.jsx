e6aff1f1e31823ded4937de99f1e146d688a0629
ayodiya <devayodiya@gmail.com>
2021-09-16 11:21:28 +0100

    fix: remove recipient input field

--- frontend/src/components/NoticeBoard/noticeBoardComponent/CreateNotice.jsx
+++ frontend/src/components/NoticeBoard/noticeBoardComponent/CreateNotice.jsx
@@ -6,9 +6,6 @@ import Hidden from '@material-ui/core/Hidden'
 import TextField from '@material-ui/core/TextField'
 import { EditorState, convertToRaw } from 'draft-js'
 import { Editor } from 'react-draft-wysiwyg'
-import FormControl from '@material-ui/core/FormControl'
-import Select from '@material-ui/core/Select'
-import MenuItem from '@material-ui/core/MenuItem'
 import draftToHtml from 'draftjs-to-html'
 import { makeStyles } from '@material-ui/core/styles'
 
@@ -122,30 +119,6 @@ function CreateNotice () {
                     type='text'
                     variant='outlined'
                   />
-                </Box>
-                <Box width='100%' pt='30px' className={classes.form}>
-                  <Box pb='10px'>
-                    <Box fontWeight='fontWeightBold'>To:</Box>
-                  </Box>
-                  <FormControl
-                    variant='outlined'
-                    className={classes.formControl}
-                  >
-                    <Select
-                      value={values.recipient}
-                      id='recipient'
-                      name='recipient'
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                    >
-                      <MenuItem value=''>
-                        <em></em>
-                      </MenuItem>
-                      <MenuItem value={10}>Ten</MenuItem>
-                      <MenuItem value={20}>Twenty</MenuItem>
-                      <MenuItem value={30}>Thirty</MenuItem>
-                    </Select>
-                  </FormControl>
                 </Box>
               </Box>
               <Box pt='30px' pb='50px'>
