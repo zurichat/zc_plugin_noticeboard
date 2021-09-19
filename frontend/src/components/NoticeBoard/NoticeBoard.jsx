825fb0d39cd55308f3a0b0359a236b2be64492ec
unknown <franklinchibuikeominyi@gmail.com>
2021-09-19 10:19:03 -0700

    Created the email unsubscription page @Chitech

--- frontend/src/components/NoticeBoard/NoticeBoard.jsx
+++ frontend/src/components/NoticeBoard/NoticeBoard.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import './NoticeBoard.css'
-import NoticeBoardHeader from './NoticeBoardHeader'
-import AdminNotice from '../NoticeBoard/noticeBoardComponent/AdminNotice'
-import { BrowserRouter as Switch, Route } from "react-router-dom"
-import CreateNotice from './noticeBoardComponent/CreateNotice'
-import UserIntro from "../NoticeBoard/noticeBoardComponent/UserIntro component/UserIntro"
-import UserNotice from './noticeBoardComponent/UserNoticeBoard'
+import React from "react";
+import { useEffect, useState } from "react";
+import "./NoticeBoard.css";
+import NoticeBoardHeader from "./NoticeBoardHeader";
+import AdminNotice from "../NoticeBoard/noticeBoardComponent/AdminNotice";
+import { BrowserRouter as Switch, Route } from "react-router-dom";
+import CreateNotice from "./noticeBoardComponent/CreateNotice";
+import UserIntro from "../NoticeBoard/noticeBoardComponent/UserIntro component/UserIntro";
+import UserNotice from "./noticeBoardComponent/UserNoticeBoard";
+
+import EmailUnsubscription from "./EmailUnsubscriptionPage/EmailUnsubscription";
 
 function NoticeBoard() {
   return (
@@ -21,13 +24,18 @@ function NoticeBoard() {
         <Route exact path="/user-notice">
           <UserNotice />
         </Route>
+
+        <Route exact path="/unsubscribe/email/:id">
+          <EmailUnsubscription />
+        </Route>
+
         <Route exact path="/">
           <UserIntro />
         </Route>
 
         {/* <Route path="/"> 
-          this component should be created in the NoticeBoard/noticeBoard folder  remember to create a link for the View Notice Button that routes to admin-notice above <UserNotice />
-        </Route> */}
+      this component should be created in the NoticeBoard/noticeBoard folder  remember to create a link for the View Notice Button that routes to admin-notice above <UserNotice />
+    </Route> */}
       </Switch>
     </div>
   );
