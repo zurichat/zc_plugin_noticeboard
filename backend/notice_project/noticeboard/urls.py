bce2c05a3c4633d92db4cf46ea4d453fdf5365fb
Daniel Iheonu <64360587+danieldutcum@users.noreply.github.com>
2021-09-20 18:58:08 +0100

    removed the static folder

--- backend/notice_project/noticeboard/urls.py
+++ backend/notice_project/noticeboard/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
 
     path('api/v1/', include('notice.urls')),
 
-    re_path('', TemplateView.as_view(template_name = "index.html"))
+    re_path(r'^.*', TemplateView.as_view(template_name = "index.html"))
     
 ]
 
