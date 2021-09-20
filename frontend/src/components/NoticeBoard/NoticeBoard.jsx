2b64414d013e49785ecad24a122adeb589a33a14
unknown <kayodefredrickbalogun@gmail.com>
2021-09-20 19:27:54 +0100

    #532 add user to noticeboard created by @tiwasulaimon @mux @zetsuarmy @toluwalaseafolabi

--- frontend/src/components/NoticeBoard/NoticeBoard.jsx
+++ frontend/src/components/NoticeBoard/NoticeBoard.jsx
@@ -1,36 +1,48 @@
-import React from 'react'
-import './NoticeBoard.css'
-import NoticeBoardHeader from './NoticeBoardHeader'
-import AdminNotice from '../NoticeBoard/noticeBoardComponent/AdminNotice'
-import { BrowserRouter as Switch, Route } from "react-router-dom"
-import CreateNotice from './noticeBoardComponent/CreateNotice'
-import UserIntro from "../NoticeBoard/noticeBoardComponent/UserIntro component/UserIntro"
-import UserNotice from './noticeBoardComponent/UserNoticeBoard'
+import React from "react";
+import { useEffect, useState } from "react";
+import "./NoticeBoard.css";
+import NoticeBoardHeader from "./NoticeBoardHeader";
+import AdminNotice from "../NoticeBoard/noticeBoardComponent/AdminNotice";
+import { BrowserRouter as Switch, Route } from "react-router-dom";
+import CreateNotice from "./noticeBoardComponent/CreateNotice";
+import UserIntro from "../NoticeBoard/noticeBoardComponent/UserIntro component/UserIntro";
+import UserNotice from "./noticeBoardComponent/UserNoticeBoard";
+import EmailUnsubscription from "./EmailUnsubscriptionPage/EmailUnsubscription";
+import SearchResult from "./noticeBoardComponent/SearchResult";
 
 function NoticeBoard() {
-  return (
-    <div className="notice">
-      <NoticeBoardHeader />
-      <Switch>
-        <Route exact path="/create-notice">
-          <CreateNotice />
-        </Route>
-        <Route exact path="/admin-notice">
-          <AdminNotice />
-        </Route>
-        <Route exact path="/user-notice">
-          <UserNotice />
-        </Route>
-        <Route exact path="/">
-          <UserIntro />
-        </Route>
+	return (
+		<div className="notice">
+			<NoticeBoardHeader />
+			<Switch>
+				<Route exact path="/search">
+					<SearchResult />
+				</Route>
 
-        {/* <Route path="/"> 
-          this component should be created in the NoticeBoard/noticeBoard folder  remember to create a link for the View Notice Button that routes to admin-notice above <UserNotice />
-        </Route> */}
-      </Switch>
-    </div>
-  );
+				<Route exact path="/create-notice">
+					<CreateNotice />
+				</Route>
+				<Route exact path="/admin-notice">
+					<AdminNotice />
+				</Route>
+				<Route exact path="/user-notice">
+					<UserNotice />
+				</Route>
+
+				<Route exact path="/unsubscribe-email/:id">
+					<EmailUnsubscription />
+				</Route>
+
+				<Route exact path="/">
+					<UserIntro />
+				</Route>
+
+				{/* <Route path="/"> 
+      this component should be created in the NoticeBoard/noticeBoard folder  remember to create a link for the View Notice Button that routes to admin-notice above <UserNotice />
+    </Route> */}
+			</Switch>
+		</div>
+	);
 }
 
 export default NoticeBoard;
