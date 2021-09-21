76564464e2cf9176b92f8da65230ba56df4499a3
dejiborewa <ayodejiakinborewa@gmail.com>
2021-09-21 13:00:35 +0100

    landing page image test

--- backend/notice_project/frontend/src/components/NoticeBoard/noticeBoardComponent/UserIntro component/UserIntro.jsx
+++ backend/notice_project/frontend/src/components/NoticeBoard/noticeBoardComponent/UserIntro component/UserIntro.jsx
@@ -4,6 +4,7 @@ import "./UserIntro.css";
 import defaultEdit from "./default.svg";
 import { IntroButtonStyles } from "./styledComponents/IntroButtonStyles";
 import { IntroPMobileStyles } from "./styledComponents/IntroPMobileStyles";
+import Illustration from "../../../../assets/Illustration.svg";
 
 const UserIntro = () => {
   return (
@@ -11,11 +12,7 @@ const UserIntro = () => {
       <div className="UserIntro">
         <div className="UserIntro-innerContainer">
           <div className="IntroImg">
-            <img
-              className="Img"
-              alt="zuri"
-              src={require("../../../../assets/Illustration.svg").default}
-            />
+            <img className="Img" alt="zuri" src={Illustration} />
           </div>
 
           <IntroPMobileStyles>
