8aea5316893c6529b702e061c055b7da8acb2569
dejiborewa <ayodejiakinborewa@gmail.com>
2021-09-25 00:53:54 +0100

    updates

--- backend/notice_project/frontend/src/components/NoticeBoard/noticeBoardComponent/Old_Notices/oldNoticeHeader.jsx
+++ backend/notice_project/frontend/src/components/NoticeBoard/noticeBoardComponent/Old_Notices/oldNoticeHeader.jsx
@@ -1,29 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import down from "./down.svg";
 import "./oldNotices.css";
 
 function OldNoticeHeader() {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const OpenMenu = () => {
+    setOpenMenu(!openMenu);
+  };
+
   return (
     <div>
       <h1 className="old-notice-title">Older Notices</h1>
-      <div className="sort">
-        <p>
-          <span className="sort-bold">Sort:</span> Most Relevant
-          <span className="sort-arrowDown-container">
-            <img className="sort-arrowDown" src={down} alt="down" />
+      <div className="sort-oldNotice" onClick={OpenMenu}>
+        <p className="sort-oldNotice-selected">
+          <span className="sort-bold-oldNotice">Sort:</span> Newest Message
+          <span className="sort-arrowDown-container-oldNotice">
+            <img className="sort-arrowDown-oldNotice" src={down} alt="down" />
           </span>
         </p>
-      </div>
-      <div className="sort">
-        <p>
-          <span className="sort-bold">Show:</span> Most Relevant
-          <span className="sort-arrowDown-container">
-            <img className="sort-arrowDown" src={down} alt="down" />
-          </span>
-        </p>
+        {openMenu && <Menu />}
       </div>
     </div>
   );
 }
+
+const Menu = () => {
+  const sortFromOldest = (event) => {
+    /*
+    const elementList = Array.from(
+      document.getElementsByClassName("sort-oldNotice-option")
+    );
+    elementList.map((element) => element.classList.remove("focus"));
+    event.currentTarget.classList.add("focus"); */
+    const sortData = async () => {
+      //
+    }
+  };
+
+  return (
+    <div className="sortMenu-oldNotice">
+      <p className="sort-oldNotice-option focus">Newest Message</p>
+      <p
+        className="sort-oldNotice-option"
+        onClick={(event) => sortFromOldest(event)}
+      >
+        Oldest Message
+      </p>
+    </div>
+  );
+};
 
 export default OldNoticeHeader;
