40c8d79ce2eec69880aaa141b487993c80600340
Daniel Iheonu <64360587+danieldutcum@users.noreply.github.com>
2021-09-24 20:51:22 +0100

    resolved user info

--- backend/notice_project/frontend/src/App.js
+++ backend/notice_project/frontend/src/App.js
@@ -4,7 +4,7 @@ import NoticeBoard from "./components/NoticeBoard/NoticeBoard";
 import { BrowserRouter as Router } from "react-router-dom";
 import Centrifuge from "centrifuge";
 import { useEffect, useState } from "react";
-// import { GetUserInfo } from "@zuri/zuri-control";
+import { GetUserInfo } from "@zuri/zuri-control";
 
 function App() {
   
@@ -31,7 +31,12 @@ function App() {
     centrifuge.connect();
   }
 
-  // console.log(GetUserInfo(), "is the user info")
+  let userID="6146f82c845b436ea04d10e1"
+
+  let token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb29raWUiOiJNVFl6TWpVd09EQTJObnhIZDNkQlIwUlplRTVFVFROTlJGVXdXa1JCZVU5RVVtbFplbHBvVDFSSmVVMTZVVEpaWnowOWZIN1pRLUZHYTlSTExIcjczUERnWVlDX3V4NU5BMmhzWUZ6M0FheERKNlRUIiwiZW1haWwiOiJwaWRAb3h5LmNvbSIsImlkIjoiNjE0MzcwNTRkMDI4NGJjNmE5MjIzNDZiIiwib3B0aW9ucyI6eyJQYXRoIjoiLyIsIkRvbWFpbiI6IiIsIk1heEFnZSI6NzkzOTY4NjE3NSwiU2VjdXJlIjpmYWxzZSwiSHR0cE9ubHkiOmZhbHNlLCJTYW1lU2l0ZSI6MH0sInNlc3Npb25fbmFtZSI6ImY2ODIyYWY5NGUyOWJhMTEyYmUzMTBkM2FmNDVkNWM3In0.SwBlvr0h2BSas0aCPaqQaRr";
+
+  console.log(GetUserInfo())
 
 
   useEffect(() => {
