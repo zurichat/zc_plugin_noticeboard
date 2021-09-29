92646d285036237a9a30369146ffe116a9552c39
ayodiya <devayodiya@gmail.com>
2021-09-29 19:18:22 +0100

    make notices show author details

--- backend/notice_project/frontend/src/components/NoticeBoard/noticeBoardComponent/Card.jsx
+++ backend/notice_project/frontend/src/components/NoticeBoard/noticeBoardComponent/Card.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import moment from 'moment'
+
+
 import dot from "../../../assets/Ellipse135.svg";
 import AdminMenu from "./AdminNoticeMenu";
 import ViewNoticeModal from "../ViewNoticeCardModal/ViewNoticeModal";
@@ -12,6 +15,10 @@ const Card = ({ person, people }) => {
     setPersons(user);
     setOpenModal(true);
   };
+
+
+  console.log(persons)
+
 
   const months = [
     "Jan",
@@ -38,21 +45,22 @@ const Card = ({ person, people }) => {
             <div className="img-profile-container-adminNotice">
               <img
                 className="profile-pic-adminNotice"
-                src="https://images.unsplash.com/photo-1582233479366-6d38bc390a08?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmFjZXN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                src={person.author_img_url !== 'null' ? person.author_img_url : "https://images.unsplash.com/photo-1582233479366-6d38bc390a08?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmFjZXN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" }
                 alt="profile-pic"
               />
             </div>
             <div className="identity-adminNotice">
               {/* no user details in notices from the api */}
-              {/* <h6 className="name">{person.userName}</h6> */}
+              <h6 className="name">{person.author_name !== 'null' ?  person.author_name : person.author_username}</h6>
               <p className="time-date-adminNotice">
-                <span>
+             { moment(person.created).fromNow()}
+                {/* <span>
                   {currentMonth}&nbsp;{person.created.slice(8, 10)}
                 </span>
                 <span className="adminDot">
                   <img src={dot} alt="" />
-                </span>{" "}
-                <span>{person.created.substring(11, 20)}</span>
+                </span>{" "} */}
+                {/* <span>{person.created.substring(11, 20)}</span> */}
               </p>
             </div>
           </div>
