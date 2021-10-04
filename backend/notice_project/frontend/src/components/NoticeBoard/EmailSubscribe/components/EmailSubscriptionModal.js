615954f463930d424dfacfb5798cb842a7d30011
Daniel Iheonu <64360587+danieliheonu@users.noreply.github.com>
2021-10-04 07:22:30 +0100

    Revert "post notice to  subscribed email recipients only"

--- backend/notice_project/frontend/src/components/NoticeBoard/EmailSubscribe/components/EmailSubscriptionModal.js
+++ backend/notice_project/frontend/src/components/NoticeBoard/EmailSubscribe/components/EmailSubscriptionModal.js
@@ -1,8 +1,8 @@
-import  "./EmailSubscriptionModal.css";
+import classes from "./EmailSubscription.module.css";
 
 const Modal = (props) => {
   return (
-    <div  onClick={props.closeHandler}>
+    <div onClick={props.closeHandler}>
       <div className="flex-column" onClick={(e) => e.stopPropagation()}>
         {props.children}
       </div>
