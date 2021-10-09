14837a8fca68d47b6db01d8a1f19fa1d530aeb90
Daniel Iheonu <64360587+danieldutcum@users.noreply.github.com>
2021-10-09 12:04:21 +0100

    created a test directory

---
+++ backend/notice_project/notice/test/test_views.py
@@ -0,0 +1,8 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+class BookmarkTest(APITestCase):
+
+    def test_list_bookmark(self):
+        pass
