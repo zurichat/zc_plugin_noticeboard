2aa9ab24a66079ad97993198da0f4d7c9b00c3ac
Ifihan <victoriaolusheye@gmail.com>
2021-11-08 11:40:33 +0100

    made fixes

--- backend/notice_project/config/settings/production.py
+++ backend/notice_project/config/settings/production.py
@@ -1,9 +1,11 @@
 from .base import *
+from django.core.management.commands.runserver import Command as runserver
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "178.63.43.138", "staging.zuri.chat"]
 
 # zurch chat database to be configure here
 DATABASES = {}
+runserver.default_port = "22670"
